468554e4234dbbab5d6daa62e95c8bc07df97d52
KAMRANkami313 <muhammadkamrankami313@gmail.com>
2026-06-24 10:46:34 +0500

    Configured MongoDB Atlas connection and backend setup

---
+++ backend/models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  caption: {
+    type: String,
+    maxlength: 500
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Post', postSchema);
